5577389ad6634e40f49ec4c9f313fd16090fa5c8
Sharps031120 <88204893+Sharps031120@users.noreply.github.com>
2021-07-30 13:55:35 -0400

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,1 +1,15 @@
-
+void printf(char* str)
+{
+    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
+  
+    for(int i=0; str[i]!='\0';++i){for(int i=0; str[i]!='\0';++i){
+        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
+    }
+                                   
+}
+  
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{
+    printf("SimpleOS");
+    while(1);
+}      
